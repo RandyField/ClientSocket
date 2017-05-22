519c5ae46382b2f38f096d0d65c563034596a4df
RandyField <287572291@qq.com>
2017-05-22 14:40:12 +0800

    ClientSocket

---
+++ SocketClient/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace SocketClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRecord = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtChat = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(14, 37);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(248, 586);
+            this.txtInfo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "socket连接记录";
+            // 
+            // txtRecord
+            // 
+            this.txtRecord.Location = new System.Drawing.Point(350, 37);
+            this.txtRecord.Multiline = true;
+            this.txtRecord.Name = "txtRecord";
+            this.txtRecord.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtRecord.Size = new System.Drawing.Size(714, 436);
+            this.txtRecord.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(350, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "聊天记录";
+            // 
+            // txtChat
+            // 
+            this.txtChat.Location = new System.Drawing.Point(352, 499);
+            this.txtChat.Multiline = true;
+            this.txtChat.Name = "txtChat";
+            this.txtChat.Size = new System.Drawing.Size(565, 107);
+            this.txtChat.TabIndex = 4;
+            this.txtChat.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtChat_KeyPress_1);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("宋体", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnSend.Location = new System.Drawing.Point(942, 499);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(122, 107);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 635);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtChat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtRecord);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInfo);
+            this.Name = "Form1";
+            this.Text = "客户端";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRecord;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtChat;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ SocketClient/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SocketClient
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// 窗体加载事件 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitSocketClient();
+        }
+
+        Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        #region 连接
+
+
+        /// <summary>
+        /// 连接-开始对远程主机连接的异步请求
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="port"></param>
+        public void Connect(IPAddress ip, int port)
+        {
+            //远程主机的 System.Net.IPAddress。
+            //远程主机的端口号
+            //requestCallback System.AsyncCallback 委托，它引用连接操作完成时要调用的方法
+            //一个用户定义对象，其中包含连接操作的相关信息。操作完成时，此对象传递给了 requestCallback 委托。        
+            this.clientSocket.BeginConnect(ip, port, new AsyncCallback(ConnectCallback), this.clientSocket);
+        }
+
+        /// <summary>
+        /// 连接操作完成时要调用的方法  可以连接为js中回调函数
+        /// </summary>
+        /// <param name="ar">异步操作的状态</param>
+        private void ConnectCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket handler = (Socket)ar.AsyncState;
+                handler.EndConnect(ar);
+            }
+            catch (SocketException ex)
+            { }
+        }
+
+        #endregion
+
+
+        #region 发送
+
+
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        /// <param name="data"></param>
+        public void Send(string data)
+        {
+            Send(System.Text.Encoding.UTF8.GetBytes(data));
+        }
+
+        /// <summary>
+        /// 发送消息-byteData
+        /// </summary>
+        /// <param name="byteData"></param>
+        private void Send(byte[] byteData)
+        {
+            try
+            {
+                //数据byte数组的长度
+                int length = byteData.Length;
+
+                this.clientSocket.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), this.clientSocket);
+                ////头
+                //byte[] head = BitConverter.GetBytes(length);
+
+                ////待发送的数据
+                //byte[] data = new byte[head.Length + byteData.Length];
+                //Array.Copy(head, data, head.Length);
+                //Array.Copy(byteData, 0, data, head.Length, byteData.Length);
+
+                //发送
+                //this.clientSocket.BeginSend(data, 0, data.Length, 0, new AsyncCallback(SendCallback), this.clientSocket);
+            }
+            catch (SocketException ex)
+            { }
+        }
+
+        /// <summary>
+        /// 发送回调函数
+        /// </summary>
+        /// <param name="ar"></param>
+        private void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket handler = (Socket)ar.AsyncState;
+                handler.EndSend(ar);
+            }
+            catch (SocketException ex)
+            { }
+        }
+
+        #endregion
+
+
+        #region 接收
+
+        byte[] MsgBuffer = new byte[4096];
+
+        /// <summary>
+        /// 接收数据
+        /// </summary>
+        public void ReceiveData()
+        {
+            clientSocket.BeginReceive(MsgBuffer, 0, MsgBuffer.Length, 0, new AsyncCallback(ReceiveCallback), null);
+        }
+
+        /// <summary>
+        /// 接收回调函数
+        /// </summary>
+        /// <param name="ar"></param>
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                int REnd = clientSocket.EndReceive(ar);
+
+                if (REnd > 0)
+                {
+                    byte[] data = new byte[REnd];
+                    Array.Copy(MsgBuffer, 0, data, 0, REnd);
+
+                    string str = System.Text.Encoding.UTF8.GetString(data);
+                    txtRecord.Text += string.Format("{0}  服务端：{1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), str);
+                    txtRecord.Text += "\r\n";
+                    //在此次可以对data进行按需处理
+
+                    clientSocket.BeginReceive(MsgBuffer, 0, MsgBuffer.Length, 0, new AsyncCallback(ReceiveCallback), null);
+                }
+                else
+                {
+                    dispose();
+                }
+            }
+            catch (SocketException ex)
+            { }
+        }
+
+        /// <summary>
+        /// 释放资源
+        /// </summary>
+        private void dispose()
+        {
+            try
+            {
+                this.clientSocket.Shutdown(SocketShutdown.Both);
+                this.clientSocket.Close();
+            }
+            catch (Exception ex)
+            { }
+        }
+        #endregion
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public void InitSocketClient()
+        {
+            int port = 8099;
+            string host = "127.0.0.1";//服务器端ip地址
+
+            IPAddress ip = IPAddress.Parse(host);
+            //IPEndPoint ipe = new IPEndPoint(ip, port);
+            Connect(ip, port);
+            ReceiveData();
+        }
+
+        /// <summary>
+        /// 发送
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            txtChat.Text = txtChat.Text.Trim();
+            Send(txtChat.Text);
+            txtRecord.Text += string.Format("{0}  客户端：{1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), txtChat.Text);
+            txtRecord.Text += "\r\n";
+            txtChat.Text = "";
+        }
+
+        private void txtChat_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.ToString() == "\r")
+            {
+                txtChat.Text = txtChat.Text.Trim();
+                //发送
+                Send(txtChat.Text);
+                txtRecord.Text += string.Format("{0}  客服端：{1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), txtChat.Text);
+                txtRecord.Text += "\r\n";
+                txtChat.Text = "";
+                
+               
+            }
+        }
+
+        /// <summary>
+        /// 客户端建立连接 发送消息 接收消息 关闭
+        /// </summary>
+        //public void InitSocketClient()
+        //{
+        //    try
+        //    {
+        //        int port = 8099;
+        //        string host = "127.0.0.1";//服务器端ip地址
+
+        //        IPAddress ip = IPAddress.Parse(host);
+        //        IPEndPoint ipe = new IPEndPoint(ip, port);
+
+        //        Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        //        clientSocket.Connect(ipe);
+
+        //        //send message
+        //        string sendStr = "send to server : hello,ni hao";
+        //        byte[] sendBytes = Encoding.ASCII.GetBytes(sendStr);
+        //        clientSocket.Send(sendBytes);
+        //        txtInfo.Text += string.Format("{0}:客服端发送信息{1}\n", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), sendStr);
+
+        //        //receive message
+        //        string recStr = "";
+        //        byte[] recBytes = new byte[4096];
+        //        int bytes = clientSocket.Receive(recBytes, recBytes.Length, 0);
+        //        recStr += Encoding.ASCII.GetString(recBytes, 0, bytes);
+        //        txtInfo.Text += string.Format("{0}:收到服务端信息{1}\n", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), recStr);
+
+        //        clientSocket.Close();
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //        throw ex;
+        //    }
+        //}
+
+       
+    }
+}
